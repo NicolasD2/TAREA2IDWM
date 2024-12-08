cb4685adf35649906989cb232d845bca6d9e944c
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2024-12-08 19:52:53 -0300

    feat: add contact component .html

--- src/app/portafolio/Components/contact/contact.component.ts
+++ src/app/portafolio/Components/contact/contact.component.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  email: string = '';
+  message: string = '';
+  isSending: boolean = false;
+  messageSent: boolean = false;
 
+  onSubmit(event: Event):void{
+    event.preventDefault();
+    this.isSending = true;
+    setTimeout(()=>{
+      this.isSending = false;
+      this.messageSent = true;
+      this.email = '';
+      this.message= '';
+    },2000);
+  }
+  onInputEmail(event:Event):void{
+    const target = event.target as HTMLInputElement;
+    if(target){
+      this.email = target.value;
+    }
+
+  }
+  onInputMessage(event: Event):void{
+    const target = event.target as HTMLTextAreaElement;
+    if(target){
+      this.message = target.value;
+    }
+  }
 }
+
+
