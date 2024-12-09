9b7fbd86c9164d87ac3f59224fcf46c9e0577814
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2024-12-08 23:54:17 -0300

    feat: add components html's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './portafolio/Components/skills/skills.component';
+import { HomeComponent } from './portafolio/Components/home/home.component';
+import { ContactComponent } from './portafolio/Components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Necesario si estás usando componentes independientes
+  imports: [RouterOutlet, SkillsComponent, HomeComponent, ContactComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Corregido: styleUrls en plural
 })
 export class AppComponent {
   title = 'portafolio';
